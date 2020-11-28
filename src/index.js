7c9f836e07cacb46a0a291c10e2381560b841cb1
flaaaps <julianwening4@gmail.com>
2020-11-28 18:30:56 +0100

    :sparkles: Add multiple windows

--- src/index.js
+++ src/index.js
@@ -13,66 +13,104 @@ const currentAppPath = appPath(app.getAppPath());
 // Require discord rpc
 const discordRPC = require('./assets/js/discord');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
-  console.log('DONE');
-  // eslint-disable-line global-require
   app.quit();
 }
 
-const createLoginWindow = () => {
-  console.log('Starting login window');
-  // Create the browser window.
-  const loginWindow = new BrowserWindow({
+let loadingWindow;
+
+let loginWindow;
+let mainWindow;
+
+const globalWebPreferences = {
+  nodeIntegration: true,
+  enableRemoteModule: true,
+  contextIsolation: false,
+};
+
+const createLoadingWindow = async () => {
+  console.log('Starting "loading" window');
+  loadingWindow = new BrowserWindow({
+    width: 400,
+    height: 400,
+    frame: false,
+    webPreferences: globalWebPreferences,
+  });
+
+  loadingWindow.loadFile(path.join(__dirname, 'sites/loading.html'));
+
+  await discordRPC.login('777509861780226069').catch((err) => {
+    console.log(err);
+  });
+  const accessToken = await readToken(currentAppPath);
+  if (await validateDragonflyAccount(accessToken)) {
+    createMainWindow();
+    loadingWindow.close();
+  } else {
+    createLoginWindow();
+    loadingWindow.close();
+  }
+
+  // loginWindow.once('ready-to-show', async () => {
+  //   autoUpdater.checkForUpdatesAndNotify();
+  // });
+};
+
+const createLoginWindow = async () => {
+  loginWindow = new BrowserWindow({
     width: 800,
     height: 700,
     frame: false,
-    webPreferences: {
-      nodeIntegration: true,
-      enableRemoteModule: true,
-    },
+    show: false,
+    webPreferences: globalWebPreferences,
   });
 
-  // and load the index.html of the app.
   loginWindow.loadFile(path.join(__dirname, 'sites/login.html'));
 
-  discordRPC
-    .login('777509861780226069')
-    .then(async () => {
-      discordRPC.setPresence({
-        state: 'Login',
-      });
-      const accessToken = await readToken(currentAppPath);
-      validateDragonflyAccount(accessToken);
-    })
-    .catch((err) => console.log(err));
-
   loginWindow.once('ready-to-show', () => {
-    autoUpdater.checkForUpdatesAndNotify();
+    discordRPC
+      .setPresence({
+        details: 'Login',
+      })
+      .catch((err) => {
+        console.log(err, 'IN INDEX!!!');
+      });
+    loginWindow.show();
   });
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+const createMainWindow = async () => {
+  console.log('Starting "login" window');
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: globalWebPreferences,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'sites/landing.html'));
+
+  mainWindow.once('ready-to-show', async () => {
+    autoUpdater.checkForUpdatesAndNotify();
+    discordRPC
+      .setPresence({
+        details: 'Home',
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
+
 app.on('ready', () => {
-  createLoginWindow();
+  createLoadingWindow();
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createLoginWindow();
   }
@@ -85,33 +123,32 @@ ipcMain.on('drgn-auth', async (event, data) => {
   let accessToken = CryptoJS.AES.encrypt(data.token, 'secretKey').toString();
 
   const accessPath = path.join(currentAppPath, '.secrets/access.txt');
-  await ensureDirectoryExistence(accessPath);
+  await ensureDirectoryExistence(accessPath, true, 'dir');
 
   fs.writeFile(accessPath, accessToken, (err) => {
     if (err) return console.log(err);
-    console.log('SAVED');
   });
   fswin.setAttributesSync(path.join(currentAppPath, '.secrets'), { IS_HIDDEN: true });
   event.reply('drgn-auth-reply', currentAppPath);
-  discordRPC.setPresence({
-    details: 'Home',
-  });
+  discordRPC
+    .setPresence({
+      details: 'Home',
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 // read access token
 ipcMain.on('drgn-auth-read', async (event, data) => {
-  console.log(reply, 'REP');
   event.reply('drgn-auth-reply', reply);
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
-
 autoUpdater.on('update-available', () => {
-  mainWindow.webContents.send('update_available');
+  loginWindow.webContents.send('update_available');
 });
 autoUpdater.on('update-downloaded', () => {
-  mainWindow.webContents.send('update_downloaded');
+  loginWindow.webContents.send('update_downloaded');
 });
 
 ipcMain.on('restart_app', () => {
