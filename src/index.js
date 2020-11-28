1464b7273654eb37c24bcfea0179c5697f576235
flaaaps <julianwening4@gmail.com>
2020-11-28 16:44:48 +0100

    :bug: Switch auto-updater

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, autoUpdater, dialog } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('path');
 const fs = require('fs');
 const fswin = require('fswin');
