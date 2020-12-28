f801707e506e74adc6222e06ebb8dc52ffcca200
flaaaps <julianwening4@gmail.com>
2020-12-28 21:13:30 +0100

    :children_crossing: Suppress window resizing

--- src/assets/js/devtools.js
+++ src/assets/js/devtools.js
@@ -7,8 +7,14 @@ document.onkeydown = function (evt) {
         console.log(
             '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
         );
-        console.log('%cHOLD UP!', '-webkit-text-stroke: 3px #DDDDDD; color: #ff0000; font-size: 5rem; font-weight: 1000');
-        console.log('%cWait a minute...', '-webkit-text-stroke: 1px #000000; color: #ffffff; font-size: 2rem; font-weight: 800');
+        console.log(
+            '%cHOLD UP!',
+            '-webkit-text-stroke: 3px #DDDDDD; color: #ff0000; font-size: 5rem; font-weight: 1000'
+        );
+        console.log(
+            '%cWait a minute...',
+            '-webkit-text-stroke: 1px #000000; color: #ffffff; font-size: 2rem; font-weight: 800'
+        );
         console.log(
             "%cIf someone told you to copy/paste something here you have a monstrous chance you're being scammed.\n\nUnless you know exactly what you are doing, close this window immediately!",
             'color: #333; font-size: 1rem; font-weight: 800; -webkit-text-stroke: 1px #999;'
@@ -18,4 +24,8 @@ document.onkeydown = function (evt) {
             body: 'Unless you know exactly what you are doing here, close this window immediately!',
         });
     }
+
+    if (!evt) evt = event;
+    if ((evt.ctrlKey && evt.key == '-') || (evt.ctrlKey && evt.key == '+')) evt.preventDefault();
+    console.log(evt.key);
 };
