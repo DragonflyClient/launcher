6e9909e914f66497ede9af8df0af40fa612ff969
flaaaps <julianwening4@gmail.com>
2020-12-28 02:05:26 +0100

    :bug: Optionally create tmp folder

--- src/utilities/downloader.js
+++ src/utilities/downloader.js
@@ -1,15 +1,23 @@
 const axios = require('axios');
 const fs = require('fs');
+const app = require('electron').app;
+const { rootPath, ensureDirectoryExistence } = require('../utilities/path');
 
 async function downloadEditions() {
     try {
+        const workingDir = rootPath(app.getAppPath());
+        const localFile = workingDir + '\\tmp\\editions.json';
+
         const url = 'https://api.playdragonfly.net/v1/launcher/editions';
         const text = JSON.stringify((await axios.get(url)).data);
 
-        fs.writeFileSync('tmp/editions.json', text);
+        ensureDirectoryExistence(localFile, true, 'dir');
+
+        fs.writeFileSync(localFile, text);
         console.log('> Successfully downloaded Dragonfly editions');
     } catch (e) {
         console.log('> Failed to download Dragonfly editions');
+        console.error(e);
     }
 }
 
