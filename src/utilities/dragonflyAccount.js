0d3873b19ab721b214a1809d593f1e6cc489487f
flaaaps <julianwening4@gmail.com>
2020-12-28 19:32:40 +0100

    :sparkles: Handle login and token read

--- src/utilities/dragonflyAccount.js
+++ src/utilities/dragonflyAccount.js
@@ -1,10 +1,15 @@
 const axios = require('axios').default;
-const validateAuthURL = 'https://api.playdragonfly.net/v1/authentication/token';
+const baseAuthUrl = 'https://api.playdragonfly.net/v1/authentication';
 
-module.exports.validateDragonflyAccount = (token) => {
+const fs = require('fs');
+const path = require('path');
+
+const CryptoJS = require('crypto-js');
+
+module.exports.validateDragonflyAccount = token => {
     return axios
         .post(
-            validateAuthURL,
+            baseAuthUrl + '/token',
             {},
             {
                 headers: {
@@ -12,15 +17,42 @@ module.exports.validateDragonflyAccount = (token) => {
                 },
             }
         )
-        .then((res) => {
+        .then(res => {
             if (res.data.success) {
                 return true;
             } else {
                 return false;
             }
         })
-        .catch((err) => {
+        .catch(err => {
             console.log(err.response.status);
             return false;
         });
+};
+
+module.exports.dragonflyAccountLogin = loginBody => {
+    return axios
+        .post(baseAuthUrl + '/login', loginBody)
+        .then(res => {
+            console.log(res.data);
+            if (res.data.success) {
+                console.log(res.data.token, 'DRAGONFLY TOKEN LOGIN!');
+                return res.data;
+            } else {
+                console.log('No success logging into dragonfly account!');
+                return false;
+            }
+        })
+        .catch(err => {
+            return err.response.data;
+        });
+};
+
+module.exports.getDragonflyToken = appPath => {
+    try {
+        const cipherText = fs.readFileSync(path.join(appPath, '.secrets', 'access.txt')).toString();
+        const bytes = CryptoJS.AES.decrypt(cipherText, 'secretKey');
+        const token = bytes.toString(CryptoJS.enc.Utf8);
+        return token;
+    } catch (error) {}
 };
