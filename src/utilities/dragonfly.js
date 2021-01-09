56759090ec02dd1e60646458ce9fb895a35a6bd4
flaaaps <julianwening4@gmail.com>
2021-01-09 14:21:18 +0100

    :art: Format file

--- src/utilities/dragonfly.js
+++ src/utilities/dragonfly.js
@@ -1,17 +1,17 @@
-const axios = require('axios').default;
-const baseAuthUrl = 'https://api.playdragonfly.net/v1/authentication';
+const axios = require("axios").default
+const baseAuthUrl = "https://api.playdragonfly.net/v1/authentication"
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs")
+const path = require("path")
 
-const CryptoJS = require('crypto-js');
+const CryptoJS = require("crypto-js")
 
-const defaultDragonflyConfig = { editionMinecraftVersion: '1.8.8' };
+const defaultDragonflyConfig = { editionMinecraftVersion: "1.8.8" }
 
 module.exports.getDragonflyAccount = (token, validate = false) => {
     return axios
         .post(
-            baseAuthUrl + '/token',
+            baseAuthUrl + "/token",
             {},
             {
                 headers: {
@@ -21,67 +21,74 @@ module.exports.getDragonflyAccount = (token, validate = false) => {
         )
         .then(res => {
             if (res.data.success) {
-                if (validate) return true;
-                return res.data;
+                if (validate) return true
+                return res.data
             } else {
-                if (validate) return false;
-                return res.data;
+                if (validate) return false
+                return res.data
             }
         })
         .catch(err => {
-            return false;
-        });
-};
+            return false
+        })
+}
 
 module.exports.dragonflyAccountLogin = loginBody => {
     return axios
-        .post(baseAuthUrl + '/login', loginBody)
+        .post(baseAuthUrl + "/login", loginBody)
         .then(res => {
             if (res.data.success) {
-                return res.data;
+                return res.data
             } else {
-                console.log('> [Dragonfly] No success logging into account');
-                return false;
+                console.log("> [Dragonfly] No success logging into account")
+                return false
             }
         })
         .catch(err => {
-            return err.response.data;
-        });
-};
+            console.log("> [Dragonfly] Error while logging into account.")
+            if (err.message === "Network Error")
+                return {
+                    success: false,
+                    title: "Network Error",
+                    error: "It looks like you're not connected to the internet. Establish a connection and try again.",
+                }
+            return err.response.data
+        })
+}
 
 module.exports.getDragonflyToken = appPath => {
     try {
-        const cipherText = fs.readFileSync(path.join(appPath, '.secrets', 'access.txt')).toString();
-        const bytes = CryptoJS.AES.decrypt(cipherText, 'secretKey');
-        const token = bytes.toString(CryptoJS.enc.Utf8);
-        return token;
+        const cipherText = fs.readFileSync(path.join(appPath, ".secrets", "access.txt")).toString()
+        const bytes = CryptoJS.AES.decrypt(cipherText, "secretKey")
+        const token = bytes.toString(CryptoJS.enc.Utf8)
+        return token
     } catch (error) {
-        console.log('> [Dragonfly] Error while reading dragonfly token');
+        console.log("> [Dragonfly] Error while reading dragonfly token")
     }
-};
+}
 
 /* Configuration regarding dragonfly */
 module.exports.currentEditionVersion = appPath => {
     try {
-        if (!fs.existsSync(path.join(appPath, 'tmp', 'config.json'))) {
-            fs.writeFileSync(path.join(appPath, 'tmp', 'config.json'), JSON.stringify(defaultDragonflyConfig));
+        if (!fs.existsSync(path.join(appPath, "tmp", "config.json"))) {
+            fs.writeFileSync(path.join(appPath, "tmp", "config.json"), JSON.stringify(defaultDragonflyConfig))
         }
-        const content = JSON.parse(fs.readFileSync(path.join(appPath, 'tmp', 'config.json')));
-        return content.editionMinecraftVersion;
+        const content = JSON.parse(fs.readFileSync(path.join(appPath, "tmp", "config.json")))
+        return content.editionMinecraftVersion
     } catch (error) {
-        console.log('> [Dragonfly] Error while reading edition version from config');
-        return null;
+        console.log("> [Dragonfly] Error while reading edition version from config")
+        return null
     }
-};
+}
 
 module.exports.writeEditionVersion = (appPath, version) => {
     try {
-        const content = JSON.parse(fs.readFileSync(path.join(appPath, 'tmp', 'config.json')));
-        content.editionMinecraftVersion = version;
-        fs.writeFileSync(path.join(appPath, 'tmp', 'config.json'), JSON.stringify(content));
-        return true;
+        const content = JSON.parse(fs.readFileSync(path.join(appPath, "tmp", "config.json")))
+        content.editionMinecraftVersion = version
+        fs.writeFileSync(path.join(appPath, "tmp", "config.json"), JSON.stringify(content))
+        return true
     } catch (error) {
-        console.log('> [Dragonfly] Error while writing edition version to config');
-        return false;
+        console.log("> [Dragonfly] Error while writing edition version to config")
+        return false
     }
-};
+}
